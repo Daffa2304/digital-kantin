f606e1aabf6c47738feee518dea9f100cf41204f
Daffa2304 <ndaffa2304@gmail.com>
2025-08-06 10:29:01 +0700

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,14 @@
 const mysql = require("mysql2/promise"); // pakai promise biar bisa pakai async/await
 
-const db = mysql.createPool({
-  host: "localhost",
-  user: "root",       // sesuaikan dengan user MySQL kamu
-  password: "R@hasia123",       // isi jika ada password MySQL
-  database: "kantin_db2", // sesuaikan dengan nama database kamu
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-module.exports = db;
+module.exports = pool;
+
